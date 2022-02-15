e6dee7426f1d8ef16b87a78992ff0c557454222b
malakh727 <76812123+malakh727@users.noreply.github.com>
2022-02-15 21:46:55 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,7 @@
+//(1) create an array of colors 
+
+//(2)search about random variables and create 3 variables that have random number in range [0-255]
+
+//(3)create a function that returns the three variables in point 3
+
+//(4) create an object called 'car' and add 3 keys and values of your choice
