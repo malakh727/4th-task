8ad86ba6cdb4045a52c80a6e6a4c44bc6103d87e
Malak Hasan <malak.hasan727@gmail.com>
2025-08-25 01:04:42 +0300

    feat: add the game javascript and make the game work

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# 4th-task for testing out css to design a small game
+# Guess The Color Game
+
+This is a simple web game where you try to guess the correct color based on its RGB code.
+
+## How to Play
+
+- The game displays an RGB color code at the top.
+- Below, there are several colored circles.
+- Click on the circle that matches the displayed RGB color code.
+- If you guess correctly, you win! If not, try again.
+
+## Features
+
+- "Easy" mode: 3 color choices.
+- "Hard" mode: 6 color choices.
+- "New Game" button to reset and play again.
+
+## Technologies Used
+
+- HTML, CSS, and JavaScript.
+
+Open `colors.html` in your browser to play the game.
--- colors.js
+++ colors.js
@@ -1,7 +1,112 @@
-//(1) create an array of colors 
+var numCircles = 6;
+var colors = generateRandomColors(numCircles);
+var circle = document.querySelectorAll(".circle");
+var pickedColor = pickColor();
+var rgbCode = document.getElementById("theRGB");
+var messageDisplay = document.querySelector("#msg");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-//(2)search about random variables and create 3 variables that have random number in range [0-255]
 
-//(3)create a function that returns the three variables in point 3
+easyBtn.addEventListener("click", function(){
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  numCircles = 3;
+  colors = generateRandomColors(numCircles);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for(var i = 0; i < circle.length; i++) {
+    if (colors[i]) {
+      circle[i].style.background = colors[i];
+    } else {
+      circle[i].style.display = "none";
+    }
+  }
+});
 
-//(4) create an object called 'car' and add 3 keys and values of your choice
+hardBtn.addEventListener("click", function(){
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numCircles = 6;
+  colors = generateRandomColors(numCircles);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for(var i = 0; i < circle.length; i++) {
+      circle[i].style.background = colors[i];
+      circle[i].style.display = "inline-block";
+  }
+});
+
+resetButton.addEventListener("click", function(){
+  //generate all new colors
+  colors = generateRandomColors(numCircles);
+  //pick a new random color from the array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked color
+  rgbCode.textContent = pickedColor;
+  this.textContent = "New Color";
+  messageDisplay.textContent = "";
+  //change colors of circle
+  for (var i = 0; i < circle.length; i++) {
+    circle[i].style.background = colors[i];
+  }
+})
+
+rgbCode.textContent = pickedColor;
+
+for(var i = 0; i < circle.length; i++) {
+  //add initial colors to circle
+  circle[i].style.background = colors[i];
+  //add click listeners to circle
+  circle[i].addEventListener("click", function() {
+    //grab color of picked circle
+    var clickedColor = this.style.background;
+    //compare color to pickedColor
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = "Good Job!";
+      resetButton.textContent = "Play Again ?";
+      changeColors(clickedColor);
+    }else{
+      this.style.background = "white";
+      messageDisplay.textContent = "Try Again";
+    }
+  })
+}
+
+function changeColors(color){
+  //loop through all circles
+for (var i = 0; i < circle.length; i++) {
+    //change each color to match given color
+    circle[i].style.background = color;
+}
+
+}
+
+function pickColor(){
+var random = Math.floor(Math.random() * colors.length)
+return colors[random];
+}
+
+function generateRandomColors(num){
+  //make and array
+  var arr = []
+  //add num random colors to array
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor())
+    //get random color and push into array
+  }
+  //return that array
+  return arr;
+}
+
+function randomColor(){
+  //pick a "red" from 0-255
+var r = Math.floor(Math.random() * 256)
+  //pick a "green" from 0-255
+var g = Math.floor(Math.random() * 256)
+  //pick a "blue" from 0-255
+var b = Math.floor(Math.random() * 256)
+
+return "rgb(" + r +", " + g + ", " + b +")";
+}
